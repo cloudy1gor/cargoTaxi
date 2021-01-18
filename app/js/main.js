51bd712e7de488c259482d6891e6ab15c9e7e526
RedCat771 <savelyev7979@gmail.com>
2021-01-18 16:30:25 +0200

    доделай чекбокс, исправь текст в кнопках

--- app/js/main.js
+++ app/js/main.js
@@ -1,25 +1,5 @@
 $(function () {
-  $(".auto__container").slick({
-    prevArrow:
-      '<button type="button" class="slick-prev"><img src="images/dest/prev.svg" alt="#"></button>',
-    nextArrow:
-      '<button type="button" class="slick-next"><img src="images/dest/next.svg" alt="#"></button>',
-  });
-
-  $(".star").rateYo({
-    starWidth: "20px",
-    ratedFill: "#3cb458",
-    normalFill: "#ccccce",
-    readOnly: true,
-    spacing: "5px",
-  });
-
-  // wow js
-  new WOW().init();
-});
-
-// липкая шапка
-$(function () {
+  // липкая шапка
   let header = $(".menu");
   let hederHeight = header.height(); // вычисляем высоту шапки
 
@@ -35,6 +15,36 @@ $(function () {
         paddingTop: 0, // удаляю отступ у body, равный высоте шапки
       });
     }
+  });
+
+  //кнопка наверх
+  $(window).scroll(function () {
+    if ($(this).scrollTop() != 0) {
+      $(".up").fadeIn();
+    } else {
+      $(".up").fadeOut();
+    }
+  });
+
+  $(".up").click(function () {
+    $("body,html").animate({ scrollTop: 0 }, 800);
+  });
+
+  // слайдер
+  $(".auto__container").slick({
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="images/dest/prev.svg" alt="#"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="images/dest/next.svg" alt="#"></button>',
+  });
+
+  // рейтинг
+  $(".star").rateYo({
+    starWidth: "20px",
+    ratedFill: "#3cb458",
+    normalFill: "#ccccce",
+    readOnly: true,
+    spacing: "5px",
   });
 });
 
@@ -58,5 +68,24 @@ for (let anchor of anchors) {
   });
 }
 
-// wow js
-new WOW().init();
+// animated
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: "aos-init", // class applied after initialization
+  animatedClassName: "aos-animate", // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 100, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: "ease", // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
+});
